26d163cd24b8bb44e562e424cf9c8c04dff4ffee
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-24 21:28:37 +0500

    переход на избранные

--- src/page3/index.js
+++ src/page3/index.js
@@ -6,11 +6,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelector("#ingr").innerHTML = detailsObject.strMeasure1 + detailsObject.strIngredient1;
     img.style.width = '300px';
     img.style.height = '300px';
-
-    document.querySelector("#submit").addEventListener("click", (evt) => {
-        evt.preventDefault();
-        localStorage.setItem("details", JSON.stringify(detailsObject));
-        document.location.href = "/cocktail_book/src/page3/index.html";
-    
 })
-});

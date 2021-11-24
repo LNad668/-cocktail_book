64bfe3014473c3070953c469e861ec853ca08164
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2021-11-24 20:24:07 +0500

    добавила page1 page2

---
+++ src/page1/index.js
@@ -0,0 +1,28 @@
+let parent = document.getElementById("content");
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+    .then(response => response.json())
+    .then(search => {
+        console.log(search);
+        const drinks = search.drinks;
+
+        for (let i = 0; i < drinks.length; i++) {
+            let title = document.createElement("h1");
+            let ingr = document.createElement("p");
+            let img = document.createElement("img");
+            let link = document.createElement("a");
+            link.href = "/thirdproject/cocktail_book/src/page2/details.html";
+
+            link.addEventListener("click", () => {
+                localStorage.setItem("details", JSON.stringify(drinks[i]))
+             })
+
+            title.innerHTML = drinks[i].strDrink;
+            ingr.innerHTML = drinks[i].strMeasure1 + drinks[i].strIngredient1;
+            img.src = drinks[i].strDrinkThumb;
+            img.style.width = '300px';
+            img.style.height = '300px';
+            link.append(title, img);
+            parent.append(ingr, link);
+        }
+    })
+    .catch(error => console.log(error));

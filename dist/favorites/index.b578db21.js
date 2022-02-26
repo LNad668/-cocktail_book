bfe1487caa988f84accfed44cd094d5f5e8462ae
Wendy-bear <w6363980@gmail.com>
2022-02-26 22:18:53 +0300

    +

--- dist/favorites/index.b578db21.js
+++ dist/favorites/index.b578db21.js
@@ -460,26 +460,26 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"3rSHg":[function(require,module,exports) {
 let parent = document.getElementById("drinkContainer");
-for(var i = 0; i < localStorage.length; i++){
+for(let i = 0; i < localStorage.length; i++){
     // set iteration key name
-    var key = localStorage.key(i);
+    let key = localStorage.key(i);
     // use key name to retrieve the corresponding value
-    // var value = localStorage.getItem(key); 
+    // var value = localStorage.getItem(key);
     const data = JSON.parse(localStorage.getItem(key));
-    let res = document.createElement('p');
-    let img = document.createElement('img');
-    let list = document.createElement('div');
+    let res = document.createElement("p");
+    let img = document.createElement("img");
+    let list = document.createElement("div");
     list.className = "main__drinkContainer";
     list.id = "main__drinkContainer";
     parent.append(list);
-    let title = document.createElement('div');
+    let title = document.createElement("div");
     title.className = "main__titleDrink";
     title.id = "main__titleDrink";
     title.innerHTML = key;
-    res.innerHTML = data[1] + ', ' + data[2] + ', ' + data[3];
+    res.innerHTML = data[1] + ", " + data[2] + ", " + data[3];
     img.src = data[0];
-    img.style.width = '400px';
-    img.style.height = '400px';
+    img.style.width = "400px";
+    img.style.height = "400px";
     list.append(img, title, res);
 }
 
--- src/favorites/index.js
+++ src/favorites/index.js
@@ -1,28 +1,26 @@
 let parent = document.getElementById("drinkContainer");
-for (var i = 0; i < localStorage.length; i++) {
-    // set iteration key name
-    var key = localStorage.key(i);
-    // use key name to retrieve the corresponding value
-   // var value = localStorage.getItem(key); 
-   
-    const data = JSON.parse(localStorage.getItem(key))
- 
-   
-     let res =document.createElement('p');
-     let img = document.createElement('img');
+for (let i = 0; i < localStorage.length; i++) {
+  // set iteration key name
+  let key = localStorage.key(i);
+  // use key name to retrieve the corresponding value
+  // var value = localStorage.getItem(key);
 
-    let list = document.createElement('div');
-    list.className = "main__drinkContainer";
-    list.id = "main__drinkContainer";
-    parent.append(list);
-    let title = document.createElement('div');
-    title.className = "main__titleDrink";
-    title.id = "main__titleDrink";
-    title.innerHTML = key;
-    res.innerHTML = data[1] + ', ' + data[2] + ', ' + data[3];
-    img.src = data[0];
-    img.style.width = '400px';
-    img.style.height = '400px';
-    list.append(img, title, res );
-   
-  }
+  const data = JSON.parse(localStorage.getItem(key));
+
+  let res = document.createElement("p");
+  let img = document.createElement("img");
+
+  let list = document.createElement("div");
+  list.className = "main__drinkContainer";
+  list.id = "main__drinkContainer";
+  parent.append(list);
+  let title = document.createElement("div");
+  title.className = "main__titleDrink";
+  title.id = "main__titleDrink";
+  title.innerHTML = key;
+  res.innerHTML = data[1] + ", " + data[2] + ", " + data[3];
+  img.src = data[0];
+  img.style.width = "400px";
+  img.style.height = "400px";
+  list.append(img, title, res);
+}

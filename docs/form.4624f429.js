16f8165bcc88bc794421d4a4696534b79fd268e3
Wendy-bear <w6363980@gmail.com>
2022-02-26 22:47:18 +0300

    +

---
+++ docs/form.4624f429.js
@@ -0,0 +1,79 @@
+document.addEventListener("submit", (event)=>{
+    event.preventDefault();
+});
+let $f3d296dd9544cf4e$var$parent = document.getElementById("main");
+document.getElementById("create").onclick = function createDrink() {
+    let titleForm = document.getElementById("title");
+    let selectAlkoForm = document.getElementById("alko");
+    let selectNonAlkoForm = document.getElementById("nonAlko");
+    let extraIngrForm = document.getElementById("extraIngr");
+    let instructionsForm = document.getElementById("instructions");
+    document.getElementById("title-error").innerHTML = "";
+    document.getElementById("alko-error").innerHTML = "";
+    document.getElementById("nonalko-error").innerHTML = "";
+    document.getElementById("extra-error").innerHTML = "";
+    document.getElementById("instructions-error").innerHTML = "";
+    let errorsArr = [];
+    let createdDrinks = [
+        "https://i.ibb.co/q7L9Jb8/4f75c983c542e08a403ca9bfbd498710.jpg", 
+    ];
+    if (titleForm.value == "") {
+        document.getElementById("title-error").innerHTML += "Oops, something went wrong! Please fill in this field";
+        errorsArr.push(document.getElementById("title-error").innerHTML);
+    }
+    //else {
+    //   createdDrinks.push(titleForm.value);
+    // }
+    if (selectAlkoForm.value == "") {
+        document.getElementById("alko-error").innerHTML += "Oops, something went wrong!<br> Please fill in this field";
+        errorsArr.push(document.getElementById("alko-error").innerHTML);
+    } else createdDrinks.push(selectAlkoForm.value);
+    if (selectNonAlkoForm.value == "") {
+        document.getElementById("nonalko-error").innerHTML += "Oops, something went wrong! <br> Please fill in this field";
+        errorsArr.push(document.getElementById("nonalko-error").innerHTML);
+    } else createdDrinks.push(selectNonAlkoForm.value);
+    if (extraIngrForm.value == "") {
+        document.getElementById("extra-error").innerHTML += "Oops, something went wrong! <br> Please fill in this field";
+        errorsArr.push(document.getElementById("extra-error").innerHTML);
+    } else createdDrinks.push(extraIngrForm.value);
+    if (instructionsForm.value == "") {
+        document.getElementById("instructions-error").innerHTML += "Oops, something went wrong! <br> Please fill in this field";
+        errorsArr.push(document.getElementById("instructions-error").innerHTML);
+    } else createdDrinks.push(instructionsForm.value);
+    console.log(errorsArr);
+    if (errorsArr.length == 0) {
+        // Садаф, посмотри что тут в ключ берется. с таким значением ключа мне дает много напитков добавить в хранилище. Проверь у себя, видишь ли ты их. Либо попробуй в getItem через `` и ${} key их взять
+        localStorage.setItem(titleForm.value, JSON.stringify(createdDrinks)); // ключ я ставлю, который стоит в избранных
+        $f3d296dd9544cf4e$var$parent.innerHTML = "";
+        main.style.backgroundColor = "#f69a73";
+        let finishBlock = document.createElement("div");
+        finishBlock.className = "main__finishblock";
+        $f3d296dd9544cf4e$var$parent.appendChild(finishBlock);
+        let imgFinal = document.createElement("img");
+        imgFinal.src = "https://media.giphy.com/media/sXcFmzvXenHEpivJZc/giphy.gif";
+        finishBlock.appendChild(imgFinal);
+        imgFinal.focus();
+        let buttonToCollection = document.createElement("button");
+        buttonToCollection.type = "text";
+        buttonToCollection.className = "button_fav";
+        buttonToCollection.innerHTML = "My collection";
+        buttonToCollection.onclick = function() {
+            // window.location.href = '/cocktail_book/src/favorites/index.html';
+            window.location.href = "./favorites/index.html";
+        };
+        finishBlock.appendChild(buttonToCollection);
+        let buttonToMain = document.createElement("button");
+        buttonToMain.type = "text";
+        buttonToMain.className = "button_main";
+        buttonToMain.innerHTML = "Back to main";
+        buttonToMain.onclick = function() {
+            // window.location.href = '/cocktail_book/src/index.html';
+            window.location.href = "index.html";
+        };
+        finishBlock.appendChild(buttonToMain);
+    }
+    console.log(createdDrinks);
+};
+
+
+//# sourceMappingURL=form.4624f429.js.map
---
+++ docs/index.26c54bfd.js
@@ -0,0 +1,100 @@
+document.addEventListener("submit", (event)=>{
+    event.preventDefault();
+});
+let $9f189f663e39dc5f$var$parent = document.getElementById("content");
+$9f189f663e39dc5f$var$parent.innerHTML = "";
+let $9f189f663e39dc5f$var$detailsDrink = document.getElementById("drinkContainer");
+$9f189f663e39dc5f$var$detailsDrink.innerHTML = "";
+document.getElementById("poisk").onclick = function createDrink() {
+    let request = document.getElementById("request").value;
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + request).then((response)=>response.json()
+    ).then((search)=>{
+        console.log(search);
+        let parent = document.getElementById("content");
+        parent.innerHTML = "";
+        for (let item of search.drinks){
+            let list = document.createElement("div");
+            list.className = "main__drinkContainer";
+            list.id = "main__drinkContainer";
+            parent.append(list);
+            let title = document.createElement("div");
+            title.className = "main__titleDrink";
+            title.id = "main__titleDrink";
+            title.innerHTML += item.strDrink;
+            // тут некоторых элементов нету, но как проверить null ингридиентов или нет, я хз
+            let ingr1 = document.createElement("div");
+            ingr1.innerHTML += item.strMeasure1 + item.strIngredient1;
+            let ingr2 = document.createElement("div");
+            ingr2.innerHTML += item.strMeasure2 + item.strIngredient2;
+            let ingr3 = document.createElement("div");
+            ingr3.innerHTML += item.strMeasure3 + item.strIngredient3;
+            let instructions = document.createElement("div");
+            instructions.innerHTML += item.strInstructions;
+            let img = document.createElement("img");
+            img.src = item.strDrinkThumb;
+            img.style.width = "200px";
+            img.style.height = "200px";
+            list.append(img, title);
+            list.onclick = function renderDrink() {
+                fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + `${title.innerHTML}`).then((response)=>response.json()
+                ).then((rendDrinks)=>{
+                    console.log(rendDrinks);
+                    $9f189f663e39dc5f$var$detailsDrink.innerHTML = "";
+                    let drinkImgContainer = document.createElement("div");
+                    drinkImgContainer.style.width = "50%";
+                    drinkImgContainer.className = "drink-render-container";
+                    $9f189f663e39dc5f$var$detailsDrink.append(drinkImgContainer);
+                    let drinkRenderImg = document.createElement("img");
+                    drinkRenderImg.src = `${img.src}`;
+                    drinkRenderImg.style.width = "500px";
+                    drinkRenderImg.style.height = "500px";
+                    drinkImgContainer.appendChild(drinkRenderImg);
+                    let drinkDetailsContainer = document.createElement("div");
+                    drinkDetailsContainer.style.width = "50%";
+                    drinkDetailsContainer.className = "drink-render-details-container";
+                    $9f189f663e39dc5f$var$detailsDrink.append(drinkDetailsContainer);
+                    let drinkRenderTitle = document.createElement("div");
+                    drinkRenderTitle.className = "renderTitle";
+                    drinkRenderTitle.innerHTML = `${title.innerHTML}`;
+                    drinkDetailsContainer.appendChild(drinkRenderTitle);
+                    let drinkRenderIngr1 = document.createElement("div");
+                    drinkRenderIngr1.className = "renderIngr";
+                    drinkRenderIngr1.innerHTML = `${ingr1.innerHTML}`;
+                    drinkDetailsContainer.appendChild(drinkRenderIngr1);
+                    let drinkRenderIngr2 = document.createElement("div");
+                    drinkRenderIngr2.className = "renderIngr";
+                    drinkRenderIngr2.innerHTML = `${ingr2.innerHTML}`;
+                    drinkDetailsContainer.appendChild(drinkRenderIngr2);
+                    let drinkRenderIngr3 = document.createElement("div");
+                    drinkRenderIngr3.className = "renderIngr";
+                    drinkRenderIngr3.innerHTML = `${ingr3.innerHTML}`;
+                    drinkDetailsContainer.appendChild(drinkRenderIngr3);
+                    let drinkRenderInstr = document.createElement("div");
+                    drinkRenderInstr.className = "renderInstr";
+                    drinkRenderInstr.innerHTML = `${instructions.innerHTML}`;
+                    drinkDetailsContainer.appendChild(drinkRenderInstr);
+                    let favButton = document.createElement("button");
+                    favButton.className = "favButton";
+                    favButton.innerHTML = "♥";
+                    drinkDetailsContainer.appendChild(favButton);
+                    favButton.focus();
+                    favButton.onclick = function saveToFav() {
+                        let favDrinks = [];
+                        favDrinks.push(drinkRenderImg.src);
+                        favDrinks.push(drinkRenderIngr1.innerHTML);
+                        favDrinks.push(drinkRenderIngr2.innerHTML);
+                        favDrinks.push(drinkRenderIngr3.innerHTML);
+                        localStorage.setItem(drinkRenderTitle.innerHTML, JSON.stringify(favDrinks));
+                        document.location.href = //  "/cocktail_book/src/favorites/index.html";
+                        "./favorites/index.html";
+                    };
+                }).catch((err)=>console.log(err)
+                );
+            };
+        }
+    }).catch((err)=>console.log(err)
+    );
+};
+
+
+//# sourceMappingURL=index.26c54bfd.js.map
